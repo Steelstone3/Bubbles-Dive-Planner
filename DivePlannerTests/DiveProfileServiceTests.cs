37ead558aa878cfe24c400de24b4997a5a1d6688
Alex Howson <steelstone3@gmail.com>
2020-10-27 19:48:53 +0000

    Quick unit test update

--- DivePlannerTests/DiveProfileServiceTests.cs
+++ DivePlannerTests/DiveProfileServiceTests.cs
@@ -124,20 +124,16 @@ namespace DivePlannerTests
                     new double[16] { 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79 },
                     new double[16] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
                     new double[16] { 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79 })]
-
-        /*[InlineData(new double[16] { 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79 },
-                    new double[16] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
-                    new double[16] { 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79 },
-                    10,
-                    //Should be less surface interval
-                    new double[16] { 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79 },
-                    new double[16] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
-                    new double[16] { 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79 })]*/
-        public void RunStageTissuePressure(double[] mockTissuePressureNitrogen, double[] mockTissuePressureHelium, double[] mockTissuePressureTotal, int bottomTime, double[] tissuePressureNitrogenResult, double[] tissuePressureHeliumResult, double[] tissuePressureTotalResult)
+        public void RunStageTissuePressure(double[] mockTissuePressuresNitrogen, double[] mockTissuePressuresHelium, double[] mockTissuePressuresTotal, int bottomTime, double[] tissuePressureNitrogenResult, double[] tissuePressureHeliumResult, double[] tissuePressureTotalResult)
         {
             InitaliseDiveProfile();
 
+            _diveProfile.TissuePressuresNitrogen = new List<double>(mockTissuePressuresNitrogen);
+            _diveProfile.TissuePressuresHelium = new List<double>(mockTissuePressuresHelium);
+            _diveProfile.TissuePressuresTotal = new List<double>(mockTissuePressuresTotal);
+
             var diveStage = new DiveStageTissuePressure(_diveModel, _diveProfile, bottomTime);
+
 
             //Act
             for (int i = 0; i < _diveModel.CompartmentCount; i++)
@@ -152,14 +148,18 @@ namespace DivePlannerTests
         }
 
         [Theory]
-        //Should be 0 default
+        //Default values, with default expected
         [InlineData(new double[16] { 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79, 0.79 },
                     new double[16] { 1.2559, 1.0000, 0.8618, 0.7562, 0.6667, 0.5600, 0.4947, 0.4500, 0.4187, 0.3798, 0.3497, 0.3223, 0.2850, 0.2737, 0.2523, 0.2327 },
                     new double[16] { 0.5050, 0.6514, 0.7222, 0.7825, 0.8126, 0.8434, 0.8693, 0.8910, 0.9092, 0.9222, 0.9319, 0.9403, 0.9477, 0.9544, 0.9602, 0.9653 },
-                    new double[16] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 })]
+                    new double[16] { -0.2353, -0.1368, -0.0519, 0.0264, 0.1002, 0.194, 0.2567, 0.3029, 0.3376, 0.3783, 0.4103, 0.4398, 0.4786, 0.4928, 0.5163, 0.538 })]
         public void RunStageToleratedAmbientPressure(double[] mockTissuePressuresTotal, double[] mockAValues, double[] mockBValues, double[] toleratedAmbientPressuresResult)
         {
             InitaliseDiveProfile();
+
+            _diveProfile.TissuePressuresTotal = new List<double>(mockTissuePressuresTotal);
+            _diveProfile.AValues = new List<double>(mockAValues);
+            _diveProfile.BValues = new List<double>(mockBValues);
 
             var diveStage = new DiveStageToleratedAmbientPressure(_diveModel.CompartmentCount, _diveProfile);
 
@@ -169,11 +169,11 @@ namespace DivePlannerTests
                 diveStage.RunStage();
 
                 //Assert
-                Assert.Equal(toleratedAmbientPressuresResult[i], _diveProfile.ToleratedAmbientPressures[i], 2);
+                Assert.Equal(toleratedAmbientPressuresResult[i], _diveProfile.ToleratedAmbientPressures[i], 4);
             }
         }
 
-        [Fact(Skip = "Not implemented")]
+        [Fact(Skip = "Big interegration test leaving for now")]
         public void RunDiveStages()
         {
 
