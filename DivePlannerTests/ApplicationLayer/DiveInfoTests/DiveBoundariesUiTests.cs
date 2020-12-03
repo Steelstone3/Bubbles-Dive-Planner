c084e7121f8d6f971c45f90ec2ef41605d50a73d
Skippy <steelstone3@gmail.com>
2020-12-03 23:28:13 +0000

    set up test class

--- DivePlannerTests/ApplicationLayer/DiveInfoTests/DiveBoundariesUiTests.cs
+++ DivePlannerTests/ApplicationLayer/DiveInfoTests/DiveBoundariesUiTests.cs
@@ -5,13 +5,31 @@ namespace DivePlannerTests
     public class DiveBoundariesUiTests
     {
         [Fact(Skip = "Needs implementing")]
-        public void TestName()
+        public void DiveBoundariesModelCanBeSetTest()
         {
             //Arrange
 
             //Act
 
             //Assert
+        }
+
+        [Fact(Skip = "Need to implement test")]
+        public void MaxOperatingDepthRaisePropertyChangedTest()
+        {
+
+        }
+
+        [Fact(Skip = "Need to implement test")]
+        public void MaxDepthChangesOnGasChangeTest()
+        {
+
+        }
+
+        [Fact(Skip = "Need to implement test")]
+        public void MaxOperatingDepthCalculatedCorrectlyTest()
+        {
+
         }
     }
 }
