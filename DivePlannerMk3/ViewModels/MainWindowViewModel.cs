f0a803fa060e98510908b4b4924c75be778f6656
Skippy <steelstone3@gmail.com>
2021-03-02 21:56:30 +0000

    Some progress towards saving functionality need to create a DAL layer that runs a conversion down to the primitives of each subclass

--- DivePlannerMk3/ViewModels/MainWindowViewModel.cs
+++ DivePlannerMk3/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reactive;
+using Avalonia.Controls;
 using DivePlannerMk3.Controllers;
 using DivePlannerMk3.ViewModels.DiveHeader;
 using DivePlannerMk3.ViewModels.DiveInfo;
 using DivePlannerMk3.ViewModels.DivePlan;
 using DivePlannerMk3.ViewModels.DiveResult;
+using Newtonsoft.Json;
 using ReactiveUI;
 
 namespace DivePlannerMk3.ViewModels
@@ -18,6 +21,7 @@ namespace DivePlannerMk3.ViewModels
 
             CalculateDiveStepCommand = ReactiveCommand.Create(RunDiveStep, CanExecuteDiveStep); // create command
             NewCommand = ReactiveCommand.Create(CreateNewDiveSession);
+            SaveCommand = ReactiveCommand.Create(SaveDivePlannerState);
         }
 
         private DiveResultsViewModel _diveResults = new DiveResultsViewModel();
@@ -59,13 +63,18 @@ namespace DivePlannerMk3.ViewModels
         {
             get;
         }
-        
+
         public IObservable<bool> CanExecuteDiveStep
         {
             get => this.WhenAnyValue(vm => vm.DivePlan.GasMixture.SelectedGasMixture, vm => vm.DivePlan.DiveModelSelector.SelectedDiveModel, (selectedGasMixture, selectedDiveModel) => selectedGasMixture != null && selectedDiveModel != null);
         }
 
-        public ReactiveCommand<Unit, Unit> NewCommand 
+        public ReactiveCommand<Unit, Unit> NewCommand
+        {
+            get;
+        }
+
+        public ReactiveCommand<Unit, Unit> SaveCommand
         {
             get;
         }
@@ -84,6 +93,56 @@ namespace DivePlannerMk3.ViewModels
             DivePlan = new DivePlanViewModel(new DiveProfileService());
             DiveInfo = new DiveInfoViewModel();
             DiveHeader = new DiveHeaderViewModel();
+        }
+
+        private void SaveDivePlannerState()
+        {
+            //http://reference.avaloniaui.net/api/Avalonia.Controls/SaveFileDialog/
+
+            /*var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filters.Add(new FileDialogFilter() { Name = "Json", Extensions = { "json" } });
+            saveFileDialog.InitialFileName = "DivePlan";
+            var result = await saveFileDialog.ShowAsync(this);
+            if (result != null)
+            {
+    
+            }*/
+
+            string homePath = (Environment.OSVersion.Platform == PlatformID.Unix ||
+                Environment.OSVersion.Platform == PlatformID.MacOSX)
+                ? Environment.GetEnvironmentVariable("HOME")
+                : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+            //var jsonFile = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+            var jsonFile = JsonConvert.SerializeObject(DiveResults, Formatting.Indented);
+            jsonFile += JsonConvert.SerializeObject(DiveParametersResult, Formatting.Indented);
+            jsonFile += JsonConvert.SerializeObject(DivePlan, Formatting.Indented);
+            jsonFile += JsonConvert.SerializeObject(DiveInfo, Formatting.Indented);
+            jsonFile += JsonConvert.SerializeObject(DiveHeader, Formatting.Indented);
+
+            // File name  
+            //string fileName = $"{homePath}{Path.DirectorySeparatorChar}{Path.DirectorySeparatorChar}DivePlan.json";
+            string fileName = $"DivePlan.json";
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    writer.Write(jsonFile);
+                }
+            }
+            catch (UnauthorizedAccessException uaex)
+            {
+                Console.Write(uaex.Message);
+            }
+            catch (IOException ioex)
+            {
+                Console.Write(ioex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
         }
     }
 }
