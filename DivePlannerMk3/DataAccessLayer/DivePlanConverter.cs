0b35f0fc8c0bae0dcbd04291e6a6427adfb23113
Skippy <steelstone3@gmail.com>
2021-03-03 09:01:05 +0000

    Model to entity converter start

---
+++ DivePlannerMk3/DataAccessLayer/DivePlanConverter.cs
@@ -0,0 +1,38 @@
+using DivePlannerMk3.Contracts;
+using DivePlannerMk3.ViewModels.DivePlan;
+using Newtonsoft.Json;
+
+namespace DivePlannerMk3.DataAccessLayer
+{
+    public class DivePlanConverter : IDataConverter
+    {
+        private DivePlanViewModel _divePlan;
+
+        public DivePlanConverter(DivePlanViewModel divePlan)
+        {
+            _divePlan = divePlan;
+        }
+
+        public string ConvertModelToEntity()
+        {
+            return SerialiseDiveDivePlan();
+        }
+
+        private string SerialiseDiveDivePlan()
+        {
+            var jsonFile = string.Empty;
+
+            jsonFile = Newtonsoft.Json.JsonConvert.SerializeObject(_divePlan.DiveModelSelector.SelectedDiveModel, Formatting.Indented);
+            jsonFile += Newtonsoft.Json.JsonConvert.SerializeObject(_divePlan.DiveStep, Formatting.Indented);
+            jsonFile += Newtonsoft.Json.JsonConvert.SerializeObject(_divePlan.GasManagement, Formatting.Indented);
+            jsonFile += Newtonsoft.Json.JsonConvert.SerializeObject(_divePlan.GasMixture.MaximumOperatingDepth, Formatting.Indented);
+            jsonFile += Newtonsoft.Json.JsonConvert.SerializeObject(_divePlan.GasMixture.NewGasMixture, Formatting.Indented);
+            jsonFile += Newtonsoft.Json.JsonConvert.SerializeObject(_divePlan.GasMixture.SelectedGasMixture, Formatting.Indented);
+            
+            //TODO AH may not work
+            jsonFile += Newtonsoft.Json.JsonConvert.SerializeObject(_divePlan.GasMixture.GasMixtures, Formatting.Indented);
+
+            return jsonFile;
+        }
+    }
+}
