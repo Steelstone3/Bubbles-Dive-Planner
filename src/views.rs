7708783d564afb681a3aa8f6466add65c2976746
Skippy <steelstone3@gmail.com>
2021-03-17 23:41:18 +0000

    Iced version 0.1

---
+++ src/views.rs
@@ -0,0 +1,13 @@
+pub mod cylinder;
+pub mod cylinder_read_only;
+pub mod dive_planner;
+pub mod dive_stage;
+pub mod select_dive_model;
+pub mod dive_step;
+pub mod gas_management;
+pub mod gas_management_read_only;
+pub mod gas_mixture;
+pub mod gas_mixture_read_only;
+pub mod input_parser;
+pub mod menu_bar;
+pub mod result;
---
+++ src/views/gas_management.rs
@@ -0,0 +1,77 @@
+use super::input_parser::parse_input_u32;
+use crate::{commands::messages::Message, view_models::dive_planner::DivePlanner};
+use iced::widget::{text, text_input, Text, TextInput};
+
+pub struct GasManagementView<'a> {
+    pub surface_air_consumption_text: Text<'a>,
+    pub surface_air_consumption_input: TextInput<'a, Message>,
+}
+
+impl GasManagementView<'_> {
+    pub fn new(dive_planner: &DivePlanner) -> Self {
+        Self {
+            surface_air_consumption_text: text("S.A.C Rate (l/min)"),
+            surface_air_consumption_input: text_input(
+                "Enter S.A.C Rate",
+                &dive_planner
+                    .dive_stage
+                    .cylinder
+                    .gas_management
+                    .surface_air_consumption_rate
+                    .to_string(),
+            )
+            .on_input(Message::SurfaceAirConsumptionChanged),
+        }
+    }
+
+    pub fn update_surface_air_consumption_rate(surface_air_consumption: String) -> u32 {
+        parse_input_u32(surface_air_consumption, 3, 30)
+    }
+}
+
+#[cfg(test)]
+mod gas_management_view_should {
+    use super::*;
+
+    #[test]
+    fn update_surface_air_consumption_rate_by_parsing_and_validating_input_successfully() {
+        // Given
+        let expected = 12;
+        let input = "12".to_string();
+
+        // When
+        let validated_surface_air_consumption_rate =
+            GasManagementView::update_surface_air_consumption_rate(input);
+
+        // Then
+        assert_eq!(expected, validated_surface_air_consumption_rate);
+    }
+
+    #[test]
+    fn update_surface_air_consumption_rate_by_parsing_an_input_beyond_range() {
+        // Given
+        let expected = 30;
+        let input = "31".to_string();
+
+        // When
+        let validated_surface_air_consumption_rate =
+            GasManagementView::update_surface_air_consumption_rate(input);
+
+        // Then
+        assert_eq!(expected, validated_surface_air_consumption_rate);
+    }
+
+    #[test]
+    fn update_surface_air_consumption_rate_by_being_unable_to_parse_input() {
+        // Given
+        let expected = 3;
+        let input = "$%45sdg".to_string();
+
+        // When
+        let validated_surface_air_consumption_rate =
+            GasManagementView::update_surface_air_consumption_rate(input);
+
+        // Then
+        assert_eq!(expected, validated_surface_air_consumption_rate);
+    }
+}
