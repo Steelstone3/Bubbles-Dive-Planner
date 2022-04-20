4da82f946c07152ca405107e182a509e570e0867
Skippy <steelstone3@gmail.com>
2022-04-21 00:01:54 +0100

    Updated UI framework and started results part of the application

--- BubblesDivePlanner/Results/ResultViewModel.cs
+++ BubblesDivePlanner/Results/ResultViewModel.cs
@@ -1,13 +1,24 @@
 using System.Collections.Generic;
 using BubblesDivePlanner.DiveModels.DiveProfile;
 using BubblesDivePlanner.DiveStep;
+using System.Reactive;
 
 namespace BubblesDivePlanner.Results
 {
-    public class ResultViewModel : IResultModel
+    public class ResultViewModel : ReactiveObject, IResultModel
     {
-        //TODO AH Raise Property Change?
-        public IDiveStepModel DiveStepModel { get; set; }
-        public IDiveProfileModel DiveProfileModel { get; set; }
+        private IDiveStepModel _diveStepModel;
+        public IDiveStepModel DiveStepModel
+        {
+            get => _diveStepModel;
+            set => this.RaiseAndSetIfChanged(ref _diveStepModel, value);
+        }
+
+        private IDiveStepModel _diveProfileModel;
+        public IDiveStepModel DiveProfileModel
+        {
+            get => _diveProfileModel;
+            set => this.RaiseAndSetIfChanged(ref _diveProfileModel, value);
+        }
     }
 }
