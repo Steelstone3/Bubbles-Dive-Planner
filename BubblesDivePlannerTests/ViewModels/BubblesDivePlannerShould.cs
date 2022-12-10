6d0a1f7689f5f78af626e255f5c251746d93d073
Skippy <steelstone3@gmail.com>
2022-12-10 16:09:06 +0000

    Update BubblesDivePlannerShould.cs

--- BubblesDivePlannerTests/ViewModels/BubblesDivePlannerShould.cs
+++ BubblesDivePlannerTests/ViewModels/BubblesDivePlannerShould.cs
@@ -1,36 +1,7 @@
-using System.Collections.Generic;
-using BubblesDivePlanner.Models;
-using BubblesDivePlanner.ViewModels;
-using Moq;
-using Xunit;
-
 namespace BubblesDivePlannerTests.ViewModels
 {
     public class BubblesDivePlannerShould
     {
-        private readonly DivePlanViewModel divePlanViewModel = new();
-        private readonly Mock<IDivePlan> divePlan = new();
-
-        [Fact]
-        public void Initialise()
-        {
-            // Then
-            Assert.Null(divePlanViewModel.DivePlan);
-        }
-
-        [Fact]
-        public void RaisePropertyChanged()
-        {
-            // Given
-            var viewModelEvents = new List<string>();
-            divePlanViewModel.PropertyChanged += (sender, e) => viewModelEvents.Add(e.PropertyName);
-
-            // When
-            divePlanViewModel.DivePlan = divePlan.Object;
-
-            // Then
-            Assert.NotEmpty(viewModelEvents);
-            Assert.Contains(nameof(divePlanViewModel.DivePlan), viewModelEvents);
-        }
+        
     }
 }
