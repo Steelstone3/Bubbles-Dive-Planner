2de56e30f5a855f349672c87938f2626ddf225ca
Skippy <steelstone3@gmail.com>
2023-03-22 00:36:56 +0000

    Results refactoring

--- BubblesDivePlanner/Views/Results/DiveParametersUsedView.xaml.cs
+++ BubblesDivePlanner/Views/Results/DiveParametersUsedView.xaml.cs
@@ -1,7 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace BubblesDivePlanner.Views
+namespace BubblesDivePlanner.Views.Results
 {
     public class DiveParametersUsedView : UserControl
     {
