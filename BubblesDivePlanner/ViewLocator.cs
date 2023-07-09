0fce6e8f24b3da782212699d743c22b04580bad6
Skippy <steelstone3@gmail.com>
2023-07-09 21:25:56 +0100

    No warnings aloud

--- BubblesDivePlanner/ViewLocator.cs
+++ BubblesDivePlanner/ViewLocator.cs
@@ -7,21 +7,26 @@ namespace BubblesDivePlanner;
 
 public class ViewLocator : IDataTemplate
 {
-    public Control Build(object data)
+    public Control Build(object? data)
     {
+        if (data == null)
+        {
+            throw new ArgumentNullException(nameof(data));
+        }
+
         string name = data.GetType().FullName!.Replace("ViewModel", "View");
         Type? type = Type.GetType(name);
 
-        if (type != null)
+        if (type == null)
         {
-            return (Control)Activator.CreateInstance(type)!;
+            return new TextBlock { Text = "Not Found: " + name };
         }
 
-        return new TextBlock { Text = "Not Found: " + name };
+        return (Control)Activator.CreateInstance(type)!;
     }
 
-    public bool Match(object data)
+    public bool Match(object? data)
     {
-        return data is ViewModelBase;
+        return data == null ? throw new ArgumentNullException(nameof(data)) : data is ViewModelBase;
     }
 }
