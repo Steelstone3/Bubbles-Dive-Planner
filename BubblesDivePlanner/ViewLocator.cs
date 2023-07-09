3444bbb2ec8e85af765ff71f2cc77eee1d40a59e
Skippy <steelstone3@gmail.com>
2023-07-09 21:32:47 +0100

    Update ViewLocator.cs

--- BubblesDivePlanner/ViewLocator.cs
+++ BubblesDivePlanner/ViewLocator.cs
@@ -9,7 +9,7 @@ public class ViewLocator : IDataTemplate
 {
     public Control Build(object? data)
     {
-        if (data == null)
+        if (data is null)
         {
             throw new ArgumentNullException(nameof(data));
         }
@@ -17,7 +17,7 @@ public class ViewLocator : IDataTemplate
         string name = data.GetType().FullName!.Replace("ViewModel", "View");
         Type? type = Type.GetType(name);
 
-        if (type == null)
+        if (type is null)
         {
             return new TextBlock { Text = "Not Found: " + name };
         }
@@ -27,6 +27,6 @@ public class ViewLocator : IDataTemplate
 
     public bool Match(object? data)
     {
-        return data == null ? throw new ArgumentNullException(nameof(data)) : data is ViewModelBase;
+        return data is null ? throw new ArgumentNullException(nameof(data)) : data is ViewModelBase;
     }
 }
