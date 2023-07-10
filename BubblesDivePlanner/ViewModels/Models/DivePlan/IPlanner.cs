94f426b98519b4fde0dcf962b5f9184770086cb9
Skippy <steelstone3@gmail.com>
2023-07-10 09:56:18 +0100

    Update IPlanner.cs

--- BubblesDivePlanner/ViewModels/Models/DivePlan/IPlanner.cs
+++ BubblesDivePlanner/ViewModels/Models/DivePlan/IPlanner.cs
@@ -2,6 +2,10 @@ namespace BubblesDivePlanner.ViewModels.Models.DivePlan
 {
     public interface IPlanner
     {
+        // DiveModels
+        // DiveModel
+        // Cylinders
+        // Cylinder
         IDiveStep DiveStep { get; set; }
     }
 }
