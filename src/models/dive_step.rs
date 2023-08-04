3e93d62507f4a0df65f1c1823ff2397c153d7cc2
Skippy <steelstone3@gmail.com>
2023-08-04 17:32:07 +0100

    Testing model validation

--- src/models/dive_step.rs
+++ src/models/dive_step.rs
@@ -6,9 +6,26 @@ pub struct DiveStep {
     pub time: u32,
 }
 
+impl DiveStep {
+    pub fn validate(input: u32, maximum: u32) -> u32 {
+        if input > maximum {
+            return maximum;
+        }
+
+        input
+    }
+}
+
 #[cfg(test)]
 mod dive_step_should {
+    use super::*;
+
     #[test]
-    #[ignore = "not implemented"]
-    fn update() {}
+    fn validate_assignment_of_input() {
+        // When
+        let input = DiveStep::validate(101, 100);
+
+        // Then
+        assert_eq!(100, input);
+    }
 }
