508978ad6fcb8262147341707125a9ea033ccf26
Skippy <steelstone3@gmail.com>
2023-08-06 01:18:32 +0100

    Update dive_planner.rs

--- src/views/dive_planner.rs
+++ src/views/dive_planner.rs
@@ -2,10 +2,7 @@ use crate::commands::messages::Message;
 use crate::controllers::dive_stage::update_dive_profile;
 use crate::models::{dive_step::DiveStep, gas_mixture::GasMixture};
 use crate::{models::dive_stage::DiveStage, view_models::dive_planner::DivePlanner};
-use iced::widget::{
-    button, column, container, text, text_input, Column, Container, Row, Scrollable, Text,
-    TextInput,
-};
+use iced::widget::{button, column, scrollable, text, text_input};
 use iced::{Alignment, Element, Length, Sandbox};
 
 use super::input_parser::parse_input_u32;
@@ -56,107 +53,47 @@ impl Sandbox for DivePlanner {
     }
 
     fn view(&self) -> Element<Message> {
-        Column::new()
-            .align_items(Alignment::Center)
-            .width(Length::FillPortion(1))
-            .spacing(10)
-            .push(
-                Row::new()
-                    .spacing(10)
-                    // TODO Header in here possibly
-                    // .push(
-                    //     Row::new()
-                    //         .align_items(Alignment::Start)
-                    //         .spacing(10)
-                    //         .push(Text::new("Here lies the Header")),
-                    // )
-                    .push(Scrollable::new(
-                        column![
-                            text("Depth"),
-                            text_input("Enter Depth", &self.dive_stage.dive_step.depth.to_string())
-                                .width(100)
-                                .on_input(Self::Message::DepthChanged),
-                            text("Time"),
-                            text_input("Enter Time", &self.dive_stage.dive_step.time.to_string())
-                                .width(100)
-                                .on_input(Self::Message::TimeChanged),
-                            text("Oxygen"),
-                            text_input(
-                                "Enter Oxygen",
-                                &self.dive_stage.cylinder.gas_mixture.oxygen.to_string()
-                            )
-                            .width(100)
-                            .on_input(Self::Message::OxygenChanged),
-                            text("Helium"),
-                            text_input(
-                                "Enter Helium",
-                                &self.dive_stage.cylinder.gas_mixture.helium.to_string()
-                            )
-                            .width(100)
-                            .on_input(Self::Message::HeliumChanged),
-                            text("Nitrogen"),
-                            text(self.dive_stage.cylinder.gas_mixture.nitrogen),
-                            button("Calculate").on_press(Self::Message::CalculateDivePlan)
-                        ]
-                        .align_items(Alignment::Start)
-                        .spacing(10),
-                    ))
-                    .push(Scrollable::new(
-                        Column::new()
-                            .align_items(Alignment::Start)
-                            .spacing(10)
-                            .width(Length::FillPortion(4))
-                            .push(text(self.dive_stage.dive_model.dive_profile)),
-                    )),
-            )
-            .into()
-
-        // Column::new()
-        //     .align_items(Alignment::Center)
-        //     .spacing(20)
-        //     .push(
-        //         Row::new()
-        //             .align_items(Alignment::Center)
-        //             .spacing(20)
-        //             .push(
-        //                 Container::new(Text::new("Dive Parameters"))
-        //                     .width(Length::FillPortion(1))
-        //                     .height(Length::Shrink),
-        //             )
-        //             .push(
-        //                 Container::new(Text::new("Dive Results"))
-        //                     .width(Length::FillPortion(4))
-        //                     .height(Length::Shrink),
-        //             ),
-        //     )
-        //     .into()
-
-        // column![
-        //     text("Depth").size(24),
-        //     text_input("Enter Depth", &self.dive_stage.dive_step.depth.to_string())
-        //         .on_input(Self::Message::DepthChanged),
-        //     text("Time").size(24),
-        //     text_input("Enter Time", &self.dive_stage.dive_step.time.to_string())
-        //         .on_input(Self::Message::TimeChanged),
-        //     text("Oxygen").size(24),
-        //     text_input(
-        //         "Enter Oxygen",
-        //         &self.dive_stage.cylinder.gas_mixture.oxygen.to_string()
-        //     )
-        //     .on_input(Self::Message::OxygenChanged),
-        //     text("Helium").size(24),
-        //     text_input(
-        //         "Enter Helium",
-        //         &self.dive_stage.cylinder.gas_mixture.helium.to_string()
-        //     )
-        //     .on_input(Self::Message::HeliumChanged),
-        //     text("Nitrogen").size(24),
-        //     text(self.dive_stage.cylinder.gas_mixture.nitrogen).size(24),
-        //     button("Calculate").on_press(Self::Message::CalculateDivePlan),
-        //     text(self.dive_stage.dive_model.dive_profile).size(24),
-        // ]
-        // .padding(20)
-        // .align_items(Alignment::Start)
-        // .into()
+        column![iced::widget::row![scrollable(
+            column![
+                text("Depth"),
+                text_input("Enter Depth", &self.dive_stage.dive_step.depth.to_string())
+                    .width(100)
+                    .on_input(Self::Message::DepthChanged),
+                text("Time"),
+                text_input("Enter Time", &self.dive_stage.dive_step.time.to_string())
+                    .width(100)
+                    .on_input(Self::Message::TimeChanged),
+                text("Oxygen"),
+                text_input(
+                    "Enter Oxygen",
+                    &self.dive_stage.cylinder.gas_mixture.oxygen.to_string()
+                )
+                .width(100)
+                .on_input(Self::Message::OxygenChanged),
+                text("Helium"),
+                text_input(
+                    "Enter Helium",
+                    &self.dive_stage.cylinder.gas_mixture.helium.to_string()
+                )
+                .width(100)
+                .on_input(Self::Message::HeliumChanged),
+                text("Nitrogen"),
+                text(self.dive_stage.cylinder.gas_mixture.nitrogen),
+                button("Calculate").on_press(Self::Message::CalculateDivePlan)
+            ]
+            .align_items(Alignment::Start)
+            .spacing(10),
+        )]
+        .spacing(10)
+        .push(scrollable(
+            column![text(self.dive_stage.dive_model.dive_profile)]
+                .align_items(Alignment::Start)
+                .spacing(10)
+                .width(Length::FillPortion(4))
+        )),]
+        .align_items(Alignment::Center)
+        .width(Length::FillPortion(1))
+        .spacing(10)
+        .into()
     }
 }
