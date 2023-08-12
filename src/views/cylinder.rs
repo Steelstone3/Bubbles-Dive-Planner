578d498bb30a68c89b3618ad9510231f5fba7a31
Skippy <steelstone3@gmail.com>
2023-08-12 10:21:45 +0100

    Update cylinder.rs

--- src/views/cylinder.rs
+++ src/views/cylinder.rs
@@ -35,8 +35,12 @@ impl CylinderView<'_> {
         cylinder
     }
 
-    fn update_cylinder_pressure(input: String, cylinder: Cylinder) -> Cylinder {
-        todo!()
+    fn update_cylinder_pressure(cylinder_pressure: String, mut cylinder: Cylinder) -> Cylinder {
+        let cylinder_pressure_input = parse_input_u32(cylinder_pressure, 0);
+        cylinder.cylinder_pressure = validate_range(cylinder_pressure_input, 50, 300);
+        cylinder.update_initial_pressurised_cylinder_volume();
+
+        cylinder
     }
 }
 
@@ -140,5 +144,103 @@ mod cylinder_view_should {
 
         // Then
         assert_eq!(expected, validated_cylinder_volume);
+    }
+
+    #[test]
+    fn update_cylinder_pressure_by_parsing_and_validating_input_successfully() {
+        // Given
+        let expected = Cylinder {
+            cylinder_pressure: 200,
+            ..Default::default()
+        };
+        let cylinder = Cylinder {
+            ..Default::default()
+        };
+        let input = "200".to_string();
+
+        // When
+        let validated_cylinder_pressure = CylinderView::update_cylinder_pressure(input, cylinder);
+
+        // Then
+        assert_eq!(expected, validated_cylinder_pressure);
+    }
+
+    #[test]
+    fn updating_cylinder_pressure_updates_initial_pressurised_cylinder_volume() {
+        // Given
+        let expected = Cylinder {
+            cylinder_volume: 12,
+            cylinder_pressure: 200,
+            initial_pressurised_cylinder_volume: 2400,
+            ..Default::default()
+        };
+        let cylinder = Cylinder {
+            cylinder_volume: 12,
+            ..Default::default()
+        };
+        let input = "200".to_string();
+
+        // When
+        let validated_cylinder_pressure = CylinderView::update_cylinder_pressure(input, cylinder);
+
+        // Then
+        assert_eq!(expected, validated_cylinder_pressure);
+    }
+
+    #[test]
+    fn update_cylinder_pressure_by_parsing_an_input_beyond_range() {
+        // Given
+        let expected = Cylinder {
+            cylinder_pressure: 300,
+            ..Default::default()
+        };
+        let cylinder = Cylinder {
+            ..Default::default()
+        };
+        let input = "301".to_string();
+
+        // When
+        let validated_cylinder_pressure = CylinderView::update_cylinder_pressure(input, cylinder);
+
+        // Then
+        assert_eq!(expected, validated_cylinder_pressure);
+    }
+
+    #[test]
+    fn update_cylinder_pressure_by_parsing_an_input_below_range() {
+        // Given
+        let expected = Cylinder {
+            cylinder_pressure: 50,
+            ..Default::default()
+        };
+        let cylinder = Cylinder {
+            ..Default::default()
+        };
+        let input = "49".to_string();
+
+        // When
+        let validated_cylinder_pressure = CylinderView::update_cylinder_pressure(input, cylinder);
+
+        // Then
+        assert_eq!(expected, validated_cylinder_pressure);
+    }
+
+    #[test]
+    fn update_cylinder_pressure_by_being_unable_to_parse_input() {
+        // Given
+        let expected = Cylinder {
+            cylinder_pressure: 50,
+            ..Default::default()
+        };
+        let cylinder = Cylinder {
+            ..Default::default()
+        };
+        let input = "2£%^sdf".to_string();
+
+        // When
+        let validated_cylinder_pressure = CylinderView::update_cylinder_pressure(input, cylinder);
+
+        // Then
+        assert_eq!(expected, validated_cylinder_pressure);
     }
 }
