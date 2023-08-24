4255af1e088defdf1adc19b0ff62dc9757bcd13e
Skippy <steelstone3@gmail.com>
2023-08-24 16:10:16 +0100

    Version 0.5

---
+++ src/views/result.rs
@@ -0,0 +1,33 @@
+use iced::widget::text;
+use iced_aw::Card;
+
+use crate::{commands::messages::Message, models::dive_stage::DiveStage};
+
+pub struct ResultView<'a> {
+    pub result_text: Card<'a, Message>,
+}
+
+impl ResultView<'_> {
+    // TODO make a results view. This will contain the tab with the historic results
+    pub fn new(dive_stage: &DiveStage) -> Self {
+        let footer = format!(
+            "Parameters\n\nDive Step\nDepth: {} Time: {}\n\nCylinder\nO2%: {} N%: {} He: {}\nRemaining: {} Used: {}", 
+            dive_stage.dive_step.depth,
+            dive_stage.dive_step.time,
+            dive_stage.cylinder.gas_mixture.oxygen,
+            dive_stage.cylinder.gas_mixture.nitrogen,
+            dive_stage.cylinder.gas_mixture.helium,
+            dive_stage.cylinder.gas_management.remaining,
+            dive_stage.cylinder.gas_management.used,
+        ).to_string();
+
+        Self {
+            result_text: Card::new(
+                "Dive Profile",
+                text(dive_stage.dive_model.dive_model.dive_profile),
+            )
+            .foot(text(footer))
+            .width(iced::Length::Fixed(500.0)),
+        }
+    }
+}
