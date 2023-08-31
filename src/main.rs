9030e32e52607160c20e540c0ca12d267479e073
Skippy <steelstone3@gmail.com>
2023-08-31 02:11:14 +0100

    Iced version 0.6

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,6 @@ mod models;
 mod view_models;
 mod views;
 
-// TODO Make a results view
-// TODO Add a tab for historic results
-// TODO This will require an update to dive_planner to include historic_dive_stages Vec<DiveStage>
-// TODO On calculate the latest result will be pushed
-// TODO On save and load file use a "DivePlanner" which is basically the application state
-
-// TODO Add depth, time, gas mixuture and gas management information to each dive result
-// TODO E.G
-// | Dive Step #1 |
-// | Depth: 50, Time: 10 |
-// | Gas Mix: 21% O2, 10% He, 69% N |
-// | Gas Management: 2400 Initial, 1680 Remaining, 720 Used |
-// <Results Here>
-
-// TODO Add multiple dive models
-
 // TODO Theme and style it
 pub fn main() -> iced::Result {
     DivePlanner::run(Settings::default())
