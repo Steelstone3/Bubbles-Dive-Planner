6c7bdfb63c2818701e063d2948fb69f442ec9450
Skippy <steelstone3@gmail.com>
2023-09-13 12:38:33 +0100

    Select a default

--- src/models/select_cylinder.rs
+++ src/models/select_cylinder.rs
@@ -2,8 +2,17 @@ use super::cylinder::Cylinder;
 use crate::commands::selectable_cylinder::SelectableCylinder;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SelectCylinder {
     cylinders: [Cylinder; 3],
     pub selected_cylinder: Option<SelectableCylinder>,
+}
+
+impl Default for SelectCylinder {
+    fn default() -> Self {
+        Self {
+            cylinders: Default::default(),
+            selected_cylinder: Some(SelectableCylinder::Bottom),
+        }
+    }
 }
--- src/models/select_dive_model.rs
+++ src/models/select_dive_model.rs
@@ -3,8 +3,17 @@ use crate::commands::selectable_dive_model::SelectableDiveModel;
 use super::dive_model::DiveModel;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Default, Copy, Clone, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Copy, Clone, Serialize, Deserialize)]
 pub struct SelectDiveModel {
     pub dive_model_list: [DiveModel; 2],
     pub selected_dive_model: Option<SelectableDiveModel>,
+}
+
+impl Default for SelectDiveModel {
+    fn default() -> Self {
+        Self {
+            dive_model_list: Default::default(),
+            selected_dive_model: Some(SelectableDiveModel::Bulhmann),
+        }
+    }
 }
