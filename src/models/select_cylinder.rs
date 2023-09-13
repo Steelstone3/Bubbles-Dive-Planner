37933962042c66021e8becafb0e5553be12bf03f
Skippy <70329398+Steelstone3@users.noreply.github.com>
2023-09-13 14:53:17 +0100

    Iced version 1.0

--- src/models/select_cylinder.rs
+++ src/models/select_cylinder.rs
@@ -1,7 +1,211 @@
 use super::cylinder::Cylinder;
+use crate::commands::selectable_cylinder::SelectableCylinder;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SelectCylinder {
-    cylinders: Vec<Cylinder>,
+    pub cylinders: [Cylinder; 3],
+    pub selected_cylinder: Option<SelectableCylinder>,
+}
+
+impl Default for SelectCylinder {
+    fn default() -> Self {
+        Self {
+            cylinders: Default::default(),
+            selected_cylinder: Some(SelectableCylinder::Bottom),
+        }
+    }
+}
+
+impl SelectCylinder {
+    pub fn update_cylinder_selected(
+        &mut self,
+        selectable_cylinder: SelectableCylinder,
+        cylinder: Cylinder,
+    ) {
+        self.selected_cylinder = Some(selectable_cylinder);
+
+        match selectable_cylinder {
+            SelectableCylinder::Bottom => {
+                self.selected_cylinder = Some(selectable_cylinder);
+                self.cylinders[0] = cylinder;
+            }
+            SelectableCylinder::Decompression => {
+                self.selected_cylinder = Some(selectable_cylinder);
+                self.cylinders[1] = cylinder;
+            }
+            SelectableCylinder::Descend => {
+                self.selected_cylinder = Some(selectable_cylinder);
+                self.cylinders[2] = cylinder;
+            }
+        }
+    }
+
+    pub fn on_cylinder_selected(
+        &mut self,
+        selectable_cylinder: SelectableCylinder,
+        cylinder: &mut Cylinder,
+    ) {
+        self.selected_cylinder = Some(selectable_cylinder);
+
+        match selectable_cylinder {
+            SelectableCylinder::Bottom => {
+                self.selected_cylinder = Some(selectable_cylinder);
+                *cylinder = self.cylinders[0]
+            }
+            SelectableCylinder::Decompression => {
+                self.selected_cylinder = Some(selectable_cylinder);
+                *cylinder = self.cylinders[1]
+            }
+            SelectableCylinder::Descend => {
+                self.selected_cylinder = Some(selectable_cylinder);
+                *cylinder = self.cylinders[2]
+            }
+        }
+    }
+
+    pub fn assign_cylinder(&mut self, cylinder: Cylinder) {
+        match self.selected_cylinder.unwrap() {
+            SelectableCylinder::Bottom => {
+                self.cylinders[0] = cylinder;
+            }
+            SelectableCylinder::Decompression => {
+                self.cylinders[1] = cylinder;
+            }
+            SelectableCylinder::Descend => {
+                self.cylinders[2] = cylinder;
+            }
+        }
+    }
+
+    pub fn is_read_only(&mut self) {
+        self.cylinders[0].is_read_only = true;
+        self.cylinders[1].is_read_only = true;
+        self.cylinders[2].is_read_only = true;
+    }
+}
+
+#[cfg(test)]
+mod select_cylinder_should {
+    use crate::models::{gas_management::GasManagement, gas_mixture::GasMixture};
+
+    use super::*;
+
+    #[test]
+    fn set_cylinders_to_read_only() {
+        // Given
+        let mut select_cylinder = SelectCylinder {
+            cylinders: Default::default(),
+            selected_cylinder: Default::default(),
+        };
+
+        // When
+        select_cylinder.is_read_only();
+
+        // Then
+        assert!(select_cylinder.cylinders[0].is_read_only);
+        assert!(select_cylinder.cylinders[1].is_read_only);
+        assert!(select_cylinder.cylinders[2].is_read_only);
+    }
+
+    #[test]
+    fn assign_to_the_selected_cylinder() {
+        // Given
+        let mut select_cylinder = SelectCylinder {
+            cylinders: Default::default(),
+            selected_cylinder: Some(SelectableCylinder::Bottom),
+        };
+        let cylinder = Cylinder {
+            is_read_only: true,
+            volume: 12,
+            pressure: 200,
+            initial_pressurised_cylinder_volume: 2400,
+            gas_mixture: GasMixture {
+                oxygen: 21,
+                helium: 10,
+                nitrogen: 69,
+                maximum_operating_depth: 56.67,
+            },
+            gas_management: GasManagement {
+                remaining: 1680,
+                used: 720,
+                surface_air_consumption_rate: 12,
+            },
+        };
+
+        // When
+        select_cylinder.assign_cylinder(cylinder);
+
+        // Then
+        assert_eq!(cylinder, select_cylinder.cylinders[0]);
+        assert_ne!(cylinder, select_cylinder.cylinders[1]);
+        assert_ne!(cylinder, select_cylinder.cylinders[2]);
+    }
+
+    #[test]
+    fn update_cylinder_setup_parameters_on_selection_changed() {
+        // Given
+        let mut cylinder = Cylinder {
+            ..Default::default()
+        };
+        let expected_cylinder = Cylinder {
+            is_read_only: true,
+            volume: 12,
+            pressure: 200,
+            initial_pressurised_cylinder_volume: 2400,
+            gas_mixture: GasMixture {
+                oxygen: 21,
+                helium: 10,
+                nitrogen: 69,
+                maximum_operating_depth: 56.67,
+            },
+            gas_management: GasManagement {
+                remaining: 1680,
+                used: 720,
+                surface_air_consumption_rate: 12,
+            },
+        };
+        let mut select_cylinder = SelectCylinder {
+            cylinders: [expected_cylinder, Default::default(), Default::default()],
+            selected_cylinder: Some(SelectableCylinder::Bottom),
+        };
+
+        // When
+        select_cylinder.on_cylinder_selected(select_cylinder.selected_cylinder.unwrap(), &mut cylinder);
+
+        // Then
+        assert_eq!(cylinder, select_cylinder.cylinders[0]);
+    }
+
+    #[test]
+    fn update_the_selected_cylinder() {
+        // Given
+        let mut select_cylinder = SelectCylinder {
+            cylinders: Default::default(),
+            selected_cylinder: Some(SelectableCylinder::Bottom),
+        };
+        let cylinder = Cylinder {
+            is_read_only: true,
+            volume: 12,
+            pressure: 200,
+            initial_pressurised_cylinder_volume: 2400,
+            gas_mixture: GasMixture {
+                oxygen: 21,
+                helium: 10,
+                nitrogen: 69,
+                maximum_operating_depth: 56.67,
+            },
+            gas_management: GasManagement {
+                remaining: 1680,
+                used: 720,
+                surface_air_consumption_rate: 12,
+            },
+        };
+
+        // When
+        select_cylinder.update_cylinder_selected(select_cylinder.selected_cylinder.unwrap(), cylinder);
+
+        // Then
+        assert_eq!(cylinder, select_cylinder.cylinders[0]);
+    }
 }
