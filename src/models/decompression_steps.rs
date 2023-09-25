3e3f5964531c5c7f968ec2ea319d1e8655828978
Skippy <steelstone3@gmail.com>
2023-09-25 14:56:01 +0100

    update

--- src/models/decompression_steps.rs
+++ src/models/decompression_steps.rs
@@ -1,6 +1,7 @@
 use crate::models::dive_step::DiveStep;
 use serde::{Deserialize, Serialize};
 
+// TODO PRIORITY add a is_visible: bool to this model which is set to true when dive_steps has values
 #[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
 pub struct DecompressionSteps {
     pub dive_steps: Vec<DiveStep>,
