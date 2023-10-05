0e0559a729fbc1cc2274bfce8fa53a95fb654d47
Skippy <70329398+Steelstone3@users.noreply.github.com>
2023-10-06 00:20:24 +0100

    Iced version 1.3.2

--- src/models/dive_stage.rs
+++ src/models/dive_stage.rs
@@ -23,40 +23,41 @@ impl DiveStage {
         let mut dive_steps = vec![];
         let mut dive_stage = *self;
 
-        // TODO Refactor this into methods
-        // shortcut
         if dive_stage.dive_model.dive_profile.dive_ceiling <= 0.0 {
             return Default::default();
         }
 
-        // while is_in_decompression
         while dive_stage.dive_model.dive_profile.dive_ceiling > 0.0 {
-            // create a dive step at the nearest decompression depth
-            dive_stage.dive_step = DiveStep {
-                depth: DiveStage::find_nearest_decompression_depth(
-                    dive_stage.dive_model.dive_profile.dive_ceiling,
-                ),
-                time: 1,
-            };
-
-            // calculate the time at depth using a simulated dive_profile
-            dive_stage = DiveStage::calculate_decompression_time_at_depth(dive_stage);
-
-            // add the decompression step to the list
-            dive_steps.push(dive_stage.dive_step);
+            Self::add_decompression_step(&mut dive_stage, &mut dive_steps);
         }
 
         dive_steps
     }
 
-    fn find_nearest_decompression_depth(dive_ceiling: f32) -> u32 {
+    fn add_decompression_step(dive_stage: &mut DiveStage, dive_steps: &mut Vec<DiveStep>) {
+        dive_stage.dive_step = DiveStage::find_nearest_decompression_depth(
+            dive_stage.dive_model.dive_profile.dive_ceiling,
+        );
+
+        *dive_stage = DiveStage::calculate_decompression_time_at_depth(*dive_stage);
+
+        dive_steps.push(dive_stage.dive_step);
+    }
+
+    fn find_nearest_decompression_depth(dive_ceiling: f32) -> DiveStep {
         let step_interval = 3;
 
         if dive_ceiling <= 0.0 {
-            return 0;
+            return DiveStep::default();
         }
 
-        (dive_ceiling / (step_interval as f32)).ceil() as u32 * step_interval
+        let nearest_decompression_depth =
+            (dive_ceiling / (step_interval as f32)).ceil() as u32 * step_interval;
+
+        DiveStep {
+            depth: nearest_decompression_depth,
+            time: 1,
+        }
     }
 
     fn calculate_decompression_time_at_depth(mut dive_stage: DiveStage) -> DiveStage {
@@ -66,6 +67,7 @@ impl DiveStage {
             == DiveStage::find_nearest_decompression_depth(
                 dive_stage.dive_model.dive_profile.dive_ceiling,
             )
+            .depth
         {
             dive_stage = DiveProfile::update_dive_profile(dive_stage);
             time += 1;
@@ -269,14 +271,33 @@ mod dive_stage_should {
         assert_eq!(expected_decompression_steps, decompression_steps)
     }
 
+    #[test]
+    fn add_decompression_step() {
+        // Given
+        let expected_dive_steps = vec![DiveStep { depth: 6, time: 1 }];
+        let mut dive_stage = DiveStage {
+            dive_model: DiveModel::create_zhl16_dive_model(),
+            dive_step: dive_step_test_fixture(),
+            cylinder: cylinder_test_fixture(),
+        };
+        dive_stage.dive_model.dive_profile = dive_profile_test_fixture();
+        let mut dive_steps = vec![];
+
+        // When
+        DiveStage::add_decompression_step(&mut dive_stage, &mut dive_steps);
+
+        // Then
+        assert_eq!(expected_dive_steps, dive_steps);
+    }
+
     #[rstest]
-    #[case(-10.0, 0)]
-    #[case(0.0, 0)]
-    #[case(4.1, 6)]
-    #[case(11.6, 12)]
+    #[case(-10.0, DiveStep{ depth: 0, time: 0 })]
+    #[case(0.0, DiveStep{ depth: 0, time: 0 })]
+    #[case(4.1, DiveStep{ depth: 6, time: 1 })]
+    #[case(11.6, DiveStep{ depth: 12, time: 1 })]
     fn find_nearest_decompression_depth(
         #[case] dive_ceiling: f32,
-        #[case] expected_dive_ceiling: u32,
+        #[case] expected_dive_ceiling: DiveStep,
     ) {
         // When
         let dive_ceiling = DiveStage::find_nearest_decompression_depth(dive_ceiling);
--- src/views/parameters/dive_step.rs
+++ src/views/parameters/dive_step.rs
@@ -1,9 +1,6 @@
 use crate::{
     commands::messages::Message,
-    models::dive_step::{
-        DiveStep, MAXIMUM_DEPTH_VALUE, MAXIMUM_TIME_VALUE, MINIMUM_DEPTH_VALUE, MINIMUM_TIME_VALUE,
-    },
-    views::application::input_parser::parse_input_u32,
+    models::dive_step::DiveStep,
 };
 use iced::widget::{column, text, text_input, Column, Text, TextInput};
 
@@ -30,14 +27,6 @@ impl DiveStepView<'_> {
         .padding(10.0)
     }
 
-    pub fn update_depth(depth: String) -> u32 {
-        parse_input_u32(depth, MINIMUM_DEPTH_VALUE, MAXIMUM_DEPTH_VALUE)
-    }
-
-    pub fn update_time(time: String) -> u32 {
-        parse_input_u32(time, MINIMUM_TIME_VALUE, MAXIMUM_TIME_VALUE)
-    }
-
     fn new<'a>(dive_step: &DiveStep) -> DiveStepView<'a> {
         DiveStepView {
             dive_step_text: text("Dive Step"),
@@ -48,88 +37,5 @@ impl DiveStepView<'_> {
             time_input: text_input("Enter Time", &dive_step.time.to_string())
                 .on_input(Message::TimeChanged),
         }
-    }
-}
-
-#[cfg(test)]
-mod dive_step_view_should {
-    use super::*;
-
-    #[test]
-    fn update_depth_by_parsing_and_validating_input_successfully() {
-        // Given
-        let expected = 50;
-        let input = "50".to_string();
-
-        // When
-        let validated_depth = DiveStepView::update_depth(input);
-
-        // Then
-        assert_eq!(expected, validated_depth);
-    }
-
-    #[test]
-    fn update_depth_by_parsing_an_input_beyond_range() {
-        // Given
-        let expected = 100;
-        let input = "101".to_string();
-
-        // When
-        let validated_depth = DiveStepView::update_depth(input);
-
-        // Then
-        assert_eq!(expected, validated_depth);
-    }
-
-    #[test]
-    fn update_depth_by_being_unable_to_parse_input() {
-        // Given
-        let expected = 1;
-        let input = "$%45sdg".to_string();
-
-        // When
-        let validated_depth = DiveStepView::update_depth(input);
-
-        // Then
-        assert_eq!(expected, validated_depth);
-    }
-
-    #[test]
-    fn update_time_by_parsing_and_validating_input_successfully() {
-        // Given
-        let expected = 10;
-        let input = "10".to_string();
-
-        // When
-        let validated_depth = DiveStepView::update_time(input);
-
-        // Then
-        assert_eq!(expected, validated_depth);
-    }
-
-    #[test]
-    fn update_time_by_parsing_an_input_beyond_range() {
-        // Given
-        let expected = 60;
-        let input = "61".to_string();
-
-        // When
-        let validated_depth = DiveStepView::update_time(input);
-
-        // Then
-        assert_eq!(expected, validated_depth);
-    }
-
-    #[test]
-    fn update_time_by_being_unable_to_parse_input() {
-        // Given
-        let expected = 1;
-        let input = "$£61asd".to_string();
-
-        // When
-        let validated_depth = DiveStepView::update_time(input);
-
-        // Then
-        assert_eq!(expected, validated_depth);
     }
 }
