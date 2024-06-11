764e6d90c79fb35e1045af29c6254048dd4e5d99
Skippy <steelstone3@gmail.com>
2024-06-11 01:36:04 +0100

    update

--- BubblesDivePlannerTests/Controllers/Prototypes/CylinderPrototypeShould.cs
+++ BubblesDivePlannerTests/Controllers/Prototypes/CylinderPrototypeShould.cs
@@ -1,14 +1,22 @@
+using Moq;
 using Xunit;
 
 public class CylinderPrototypeShould
 {
-    [Fact(Skip = "To Do")]
+    private readonly ICylinderPrototype cylinderPrototype = new CylinderPrototype();
+
+    [Fact]
     public void DeepClone()
     {
         // Given
+        Mock<ICylinderValidator> cylinderValidator = new();
+        Mock<ICylinderController> cylinderController = new();
+        ICylinder cylinder = new Cylinder(cylinderValidator.Object, cylinderController.Object);
 
         // When
+        ICylinder clonedCylinder = cylinderPrototype.DeepClone(cylinder);
 
         // Then
+        Assert.NotSame(cylinder, clonedCylinder);
     }
 }
