3ccf2cf2b23f9939b7360cb79df43b20c2594ebc
Skippy <steelstone3@gmail.com>
2024-06-14 05:54:40 +0100

    update

--- BubblesDivePlanner/Controllers/CylinderController.cs
+++ BubblesDivePlanner/Controllers/CylinderController.cs
@@ -2,7 +2,6 @@ public class CylinderController : ICylinderController
 {
     public ushort CalculateInitialPressurisedVolume(byte volume, ushort pressure) => (ushort)(volume * pressure);
 
-    // TODO AH Test
     public IGasUsage UpdateGasUsage(IDiveStep diveStep, IGasUsage gasUsage)
     {
         gasUsage.Used = CalculateGasUsed(diveStep, gasUsage);
@@ -13,10 +12,8 @@ public class CylinderController : ICylinderController
 
     public float CalculateNitrogen(float oxygen, float helium) => 100.0F - oxygen - helium;
 
-    // TODO AH Test
     private ushort CalculateRemainingPressurisedVolume(IGasUsage gasUsage) => gasUsage.Remaining > gasUsage.Used ? (ushort)(gasUsage.Remaining - gasUsage.Used) : (ushort)0;
 
-    // TODO AH Test
     private ushort CalculateGasUsed(IDiveStep diveStep, IGasUsage gasUsage) => (ushort)(((diveStep.Depth / 10) + 1) * diveStep.Time * gasUsage.SurfaceAirConsumptionRate);
 }
 
