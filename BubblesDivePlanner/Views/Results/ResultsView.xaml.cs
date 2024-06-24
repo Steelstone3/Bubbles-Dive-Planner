eb849df46c10566f4d5b95eaf11e9a82a6131fb1
Skippy <steelstone3@gmail.com>
2024-06-24 20:48:41 +0100

    update

---
+++ BubblesDivePlanner/Views/Results/ResultsView.xaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BubblesDivePlanner.Views.Results
+{
+    public class ResultsView : UserControl
+    {
+        public ResultsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
