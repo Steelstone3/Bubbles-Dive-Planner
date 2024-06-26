00063c03886dae57b71797e5ce2aa9595e167e49
Skippy <steelstone3@gmail.com>
2024-06-26 03:26:47 +0100

    update

--- BubblesDivePlanner/ViewModels/Header/Help.cs
+++ BubblesDivePlanner/ViewModels/Header/Help.cs
@@ -1,9 +1,28 @@
+using System.Reactive;
+using ReactiveUI;
+
 public class Help : IHelp
 {
-    
+    public Help()
+    {
+        HelpCommand = ReactiveCommand.Create(OpenHelp);
+        AboutCommand = ReactiveCommand.Create(OpenAbout);
+    }
+
+    public ReactiveCommand<Unit, Unit> HelpCommand { get; }
+    public ReactiveCommand<Unit, Unit> AboutCommand { get; }
+
+    private void OpenHelp()
+    {
+    }
+
+    private void OpenAbout()
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public interface IHelp
 {
-    
+
 }
