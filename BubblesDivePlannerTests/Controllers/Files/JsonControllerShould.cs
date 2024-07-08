5cf6a5151aa4ee0cedd971a05e465da1ec9c4fd5
Skippy <steelstone3@gmail.com>
2024-07-08 02:13:28 +0100

    update

--- BubblesDivePlannerTests/Controllers/Files/JsonControllerShould.cs
+++ BubblesDivePlannerTests/Controllers/Files/JsonControllerShould.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+using Moq;
 using Xunit;
 
 public class JsonControllerShould
@@ -6,9 +8,29 @@ public class JsonControllerShould
     public void Serialise()
     {
         // Given
+        Mock<IDiveStep> diveStep = new();
+        diveStep.Setup(ds => ds.Depth).Returns(50);
+        diveStep.Setup(ds => ds.Time).Returns(10);
+        Mock<IGasMixture> gasMixture = new();
+        gasMixture.Setup(gm => gm.Oxygen).Returns(21);
+        gasMixture.Setup(gm => gm.Nitrogen).Returns(79);
+        Mock<ICylinder> cylinder = new();
+        cylinder.Setup(c => c.Name).Returns("Air");
+        cylinder.Setup(c => c.Volume).Returns(12);
+        cylinder.Setup(c => c.Pressure).Returns(200);
+        cylinder.Setup(c => c.InitialPressurisedVolume).Returns(2400);
+        Mock<IDiveStage> diveStage = new();
+        diveStage.Setup(ds => ds.DiveModel).Returns(new Zhl16Buhlmann());
+        diveStage.Setup(ds => ds.DiveStep).Returns(diveStep.Object);
+        diveStage.Setup(ds => ds.Cylinder).Returns(cylinder.Object);
+        Mock<Result> result = new();
+        result.Setup(r => r.Results).Returns(new ObservableCollection<IDiveStage>() { diveStage.Object });
+        JsonController jsonController = new();
 
         // When
+        string serialisedResult = jsonController.Serialise(result.Object);
 
         // Then
+        Assert.Equal("[]", serialisedResult);
     }
 }
