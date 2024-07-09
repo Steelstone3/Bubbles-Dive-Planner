127a467a951ef45d62eff921de2c178487a0cc5d
Skippy <steelstone3@gmail.com>
2024-07-09 23:44:39 +0100

    update

--- BubblesDivePlanner/Controllers/Files/Serialiser/ResultSerialiser.cs
+++ BubblesDivePlanner/Controllers/Files/Serialiser/ResultSerialiser.cs
@@ -3,6 +3,9 @@ using System.Text.Json;
 public class ResultSerialiser : ISerialiser<IResult>
 {
     public string Write(IResult result) => JsonSerializer.Serialize(result);
-    public IResult Read(string json) => JsonSerializer.Deserialize<IResult>(json);
-
+    public IResult Read(string json)
+    {
+        JsonSerializerOptions jsonSerialiserOptions = new();
+        return JsonSerializer.Deserialize<IResult>(json, jsonSerialiserOptions);
+    }
 }
