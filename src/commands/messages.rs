e05d7cd71ac34b76f36451257ca4e5b3a61b4727
Skippy <steelstone3@gmail.com>
2024-07-26 03:49:41 +0100

    update

--- src/commands/messages.rs
+++ src/commands/messages.rs
@@ -1,5 +1,3 @@
-use super::{selectable_cylinder::SelectableCylinder, selectable_dive_model::SelectableDiveModel};
-
 #[derive(Debug, Clone)]
 pub enum Message {
     // MenuBar,
@@ -10,16 +8,16 @@ pub enum Message {
     // EditRedo,
     // ViewToggleCentralNervousSystemToxicityVisibility,
     // ViewToggleSelectCylinderVisibility,
-    DiveModelSelected(SelectableDiveModel),
+    // DiveModelSelected(SelectableDiveModel),
     DepthChanged(String),
-    TimeChanged(String),
-    CylinderVolumeChanged(String),
-    CylinderPressureChanged(String),
-    SurfaceAirConsumptionChanged(String),
-    OxygenChanged(String),
-    HeliumChanged(String),
-    CylinderSelected(SelectableCylinder),
-    UpdateCylinderSelected(SelectableCylinder),
-    UpdateDiveProfile,
+    // TimeChanged(String),
+    // CylinderVolumeChanged(String),
+    // CylinderPressureChanged(String),
+    // SurfaceAirConsumptionChanged(String),
+    // OxygenChanged(String),
+    // HeliumChanged(String),
+    // CylinderSelected(SelectableCylinder),
+    // UpdateCylinderSelected(SelectableCylinder),
+    // UpdateDiveProfile,
     // DecompressionUpdateDiveProfile,
 }
--- src/views/application/dive_planner.rs
+++ src/views/application/dive_planner.rs
@@ -1,7 +1,10 @@
 use crate::commands::messages::Message;
 use crate::view_models::dive_planner::DivePlanner;
 use cosmic::iced::widget::column;
-use cosmic::iced::{Element, Sandbox};
+use cosmic::iced::Sandbox;
+use cosmic::iced_core::widget::Text;
+use cosmic::iced_core::Element;
+use cosmic::iced_widget::TextInput;
 
 // use super::menu_bar::MenuBarView;
 
@@ -38,41 +41,41 @@ impl Sandbox for DivePlanner {
             // Message::ViewToggleSelectCylinderVisibility => {
             //     self.view_toggle_select_cylinder_visibility();
             // }
-            Message::DiveModelSelected(selectable_dive_model) => {
-                self.dive_model_selected(selectable_dive_model)
-            }
+            // Message::DiveModelSelected(selectable_dive_model) => {
+            //     self.dive_model_selected(selectable_dive_model)
+            // }
             Message::DepthChanged(depth) => self.dive_stage.dive_step.update_depth(depth),
-            Message::TimeChanged(time) => self.dive_stage.dive_step.update_time(time),
-            Message::CylinderVolumeChanged(cylinder_volume) => {
-                self.dive_stage
-                    .cylinder
-                    .update_cylinder_volume(cylinder_volume);
-            }
-            Message::CylinderPressureChanged(cylinder_pressure) => {
-                self.dive_stage
-                    .cylinder
-                    .update_cylinder_pressure(cylinder_pressure);
-            }
-            Message::SurfaceAirConsumptionChanged(surface_air_consumption) => self
-                .dive_stage
-                .cylinder
-                .gas_management
-                .update_surface_air_consumption_rate(surface_air_consumption),
-            Message::OxygenChanged(oxygen) => {
-                self.dive_stage.cylinder.gas_mixture.update_oxygen(oxygen)
-            }
-            Message::HeliumChanged(helium) => {
-                self.dive_stage.cylinder.gas_mixture.update_helium(helium)
-            }
-            Message::CylinderSelected(selectable_cylinder) => {
-                self.cylinder_selected(selectable_cylinder);
-            }
-            Message::UpdateCylinderSelected(selectable_cylinder) => {
-                self.update_cylinder_selected(selectable_cylinder)
-            }
-            Message::UpdateDiveProfile => {
-                self.update_dive_profile();
-            }
+            // Message::TimeChanged(time) => self.dive_stage.dive_step.update_time(time),
+            // Message::CylinderVolumeChanged(cylinder_volume) => {
+            //     self.dive_stage
+            //         .cylinder
+            //         .update_cylinder_volume(cylinder_volume);
+            // }
+            // Message::CylinderPressureChanged(cylinder_pressure) => {
+            //     self.dive_stage
+            //         .cylinder
+            //         .update_cylinder_pressure(cylinder_pressure);
+            // }
+            // Message::SurfaceAirConsumptionChanged(surface_air_consumption) => self
+            //     .dive_stage
+            //     .cylinder
+            //     .gas_management
+            //     .update_surface_air_consumption_rate(surface_air_consumption),
+            // Message::OxygenChanged(oxygen) => {
+            //     self.dive_stage.cylinder.gas_mixture.update_oxygen(oxygen)
+            // }
+            // Message::HeliumChanged(helium) => {
+            //     self.dive_stage.cylinder.gas_mixture.update_helium(helium)
+            // }
+            // Message::CylinderSelected(selectable_cylinder) => {
+            //     self.cylinder_selected(selectable_cylinder);
+            // }
+            // Message::UpdateCylinderSelected(selectable_cylinder) => {
+            //     self.update_cylinder_selected(selectable_cylinder)
+            // }
+            // Message::UpdateDiveProfile => {
+            //     self.update_dive_profile();
+            // }
             // Message::DecompressionUpdateDiveProfile => {
             //     self.decompression_update_dive_profile();
             // }
@@ -87,28 +90,34 @@ impl Sandbox for DivePlanner {
 
     //     column![].into()
     // }
-    
-    fn view(&self) -> Element<'_, Self::Message, cosmic::iced::Theme> {
-        column![].into()
+
+    fn view(&self) -> Element<'_, Message, cosmic::iced::Theme, cosmic::iced::Renderer> {
+        column!()
+            .push(Text::new("Depth"))
+            .push(
+                TextInput::new("Enter Depth", &self.dive_stage.dive_step.depth.to_string())
+                    .on_input(Message::DepthChanged),
+            )
+            .push(Text::new("Time"))
+            .into()
     }
 
     fn theme(&self) -> cosmic::iced::Theme {
         cosmic::iced::Theme::Dark
     }
-    
+
     fn style(&self) -> cosmic::iced::theme::Application {
         cosmic::iced::theme::Application::Default
     }
-    
+
     fn scale_factor(&self) -> f64 {
         1.0
     }
-    
+
     fn run(settings: cosmic::iced::Settings<()>) -> Result<(), cosmic::iced::Error>
     where
         Self: 'static + Sized,
     {
         <Self as cosmic::iced::Application>::run(settings)
     }
-    
 }
