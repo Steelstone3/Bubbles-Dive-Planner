1e2c6011a7557731c8b7a86217d56aeb68111d7b
Skippy <steelstone3@gmail.com>
2024-07-30 05:24:54 +0100

    update

--- src/views/information/dive_boundaries.rs
+++ src/views/information/dive_boundaries.rs
@@ -1,9 +1,46 @@
 use iced::widget::{column, text};
+use iced_aw::widgets::Card;
 
 use crate::{commands::messages::Message, view_models::dive_planner::DivePlanner};
 
 impl DivePlanner {
     pub fn dive_boundaries_view(&self) -> iced::widget::Column<Message> {
-        column!(text("Dive Boundaries")).spacing(10)
+        // let maximum_operating_depth = dive_planner
+        //             .dive_stage
+        //             .cylinder
+        //             .gas_mixture
+        //             .display_maximum_operating_depth();
+        //         let dive_ceiling = dive_planner
+        //             .dive_stage
+        //             .dive_model
+        //             .dive_profile
+        //             .display_dive_ceiling();
+
+        //         DiveInformationView {
+        //             dive_information_text: Card::new(
+        //                 "Dive Information",
+        //                 text(format!(
+        //                     "{}Dive Boundaries\n\n{}\n{}",
+        //                     &dive_planner.cns_toxicity, maximum_operating_depth, dive_ceiling
+        //                 )),
+        //             )
+        //             .width(iced::Length::Fixed(500.0)),
+        //             decompression_steps: DecompressionStepsView::build_view(dive_planner),
+
+        column!(Card::new(
+            "Dive Boundaries",
+            text(format!(
+                "{}\n{}",
+                self.dive_stage
+                    .cylinder
+                    .gas_mixture
+                    .display_maximum_operating_depth(),
+                self.dive_stage
+                    .dive_model
+                    .dive_profile
+                    .display_dive_ceiling()
+            ))
+        ))
+        .spacing(10)
     }
 }
