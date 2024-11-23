cac8ba7929dd3d25e55f5fc727a4445ba34eb579
Skippy <steelstone3@gmail.com>
2024-11-23 13:26:28 +0000

    version-23-11-24

--- src/views/plan/dive_step.rs
+++ src/views/plan/dive_step.rs
@@ -1,11 +1,10 @@
 use crate::{commands::messages::Message, models::dive_planner::DivePlanner};
 use iced::widget::{column, text, text_input, Column};
+use iced_aw::Card;
 
 impl DivePlanner {
     pub fn dive_step_view(&self) -> Column<Message> {
-        column!()
-            .push(text("Dive Step"))
-            .spacing(10)
+        let contents = column!()
             .push(text("Depth"))
             .spacing(10)
             .push(
@@ -19,6 +18,8 @@ impl DivePlanner {
                 text_input("Enter Time", &self.dive_stage.dive_step.time.to_string())
                     .on_input(Message::TimeChanged),
             )
-            .spacing(10)
+            .spacing(10);
+
+        column!().push(Card::new("Dive Step", contents))
     }
 }
