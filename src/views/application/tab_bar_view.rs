fc5a76041f7083d84f07c8082effa918f77bba53
Skippy <steelstone3@gmail.com>
2024-11-24 06:15:28 +0000

    version-24-11-24

--- src/views/application/tab_bar_view.rs
+++ src/views/application/tab_bar_view.rs
@@ -8,63 +8,23 @@ use crate::{
 
 impl DivePlanner {
     pub fn tab_bar_view(&self) -> Column<Message> {
-        // TODO AH extract repetition
         match self.tab_identifier {
             TabIdentifier::Plan => {
-                let tab_bar = TabBar::new(Message::TabSelected)
-                    .push(
-                        TabIdentifier::Plan,
-                        TabLabel::IconText('p', "Plan".to_string()),
-                    )
-                    .push(
-                        TabIdentifier::Information,
-                        TabLabel::IconText('i', "Information".to_string()),
-                    )
-                    .push(
-                        TabIdentifier::Results,
-                        TabLabel::IconText('r', "Results".to_string()),
-                    )
-                    .set_active_tab(&TabIdentifier::Plan);
+                let tab_bar = selected_tab_bar(&TabIdentifier::Plan);
 
                 let contents = Scrollable::new(column!().push(self.plan_view()));
 
                 column!(self.menu_view(), tab_bar, contents)
             }
             TabIdentifier::Information => {
-                let tab_bar = TabBar::new(Message::TabSelected)
-                    .push(
-                        TabIdentifier::Plan,
-                        TabLabel::IconText('p', "Plan".to_string()),
-                    )
-                    .push(
-                        TabIdentifier::Information,
-                        TabLabel::IconText('i', "Information".to_string()),
-                    )
-                    .push(
-                        TabIdentifier::Results,
-                        TabLabel::IconText('r', "Results".to_string()),
-                    )
-                    .set_active_tab(&TabIdentifier::Information);
+                let tab_bar = selected_tab_bar(&TabIdentifier::Information);
 
                 let contents = Scrollable::new(column!().push(self.information_view()));
 
                 column!(self.menu_view(), tab_bar, contents)
             }
             TabIdentifier::Results => {
-                let tab_bar = TabBar::new(Message::TabSelected)
-                    .push(
-                        TabIdentifier::Plan,
-                        TabLabel::IconText('p', "Plan".to_string()),
-                    )
-                    .push(
-                        TabIdentifier::Information,
-                        TabLabel::IconText('i', "Information".to_string()),
-                    )
-                    .push(
-                        TabIdentifier::Results,
-                        TabLabel::IconText('r', "Results".to_string()),
-                    )
-                    .set_active_tab(&TabIdentifier::Results);
+                let tab_bar = selected_tab_bar(&TabIdentifier::Results);
 
                 let contents = Scrollable::new(column!().push(self.results_view()));
 
@@ -72,4 +32,22 @@ impl DivePlanner {
             }
         }
     }
+}
+
+fn selected_tab_bar(active_tab: &TabIdentifier) -> TabBar<'static, Message, TabIdentifier> {
+    let tab_bar = TabBar::new(Message::TabSelected)
+        .push(
+            TabIdentifier::Plan,
+            TabLabel::IconText('p', "Plan".to_string()),
+        )
+        .push(
+            TabIdentifier::Information,
+            TabLabel::IconText('i', "Information".to_string()),
+        )
+        .push(
+            TabIdentifier::Results,
+            TabLabel::IconText('r', "Results".to_string()),
+        )
+        .set_active_tab(active_tab);
+    tab_bar
 }
