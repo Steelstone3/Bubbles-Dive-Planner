86ee9c17e0907bbe209d17551d53dfcd5fb3fb05
Skippy <steelstone3@gmail.com>
2026-03-06 17:23:59 +0000

    version-06-03-26

--- src/models/application/dive_planner.rs
+++ src/models/application/dive_planner.rs
@@ -1,8 +1,11 @@
 use super::application_state::ApplicationState;
-use crate::models::{
-    information::dive_information::DiveInformation,
-    plan::{dive_planning::dive_pre_planning::DivePrePlanning, dive_stage::DiveStage},
-    result::results::DiveResults,
+use crate::{
+    application::states::selectable_dive_model::SelectableDiveModel,
+    models::{
+        information::dive_information::DiveInformation,
+        plan::{dive_planning::dive_pre_planning::DivePrePlanning, dive_stage::DiveStage},
+        result::results::DiveResults,
+    },
 };
 use serde::{Deserialize, Serialize};
 
@@ -10,6 +13,39 @@ use serde::{Deserialize, Serialize};
 pub struct DivePlanner {
     pub application_state: ApplicationState,
     pub dive_planning: DivePrePlanning,
+    pub dive_stage: DiveStage,
+    pub dive_information: DiveInformation,
+    pub dive_results: DiveResults,
+}
+
+impl DivePlanner {
+    fn new_from_load(dive_planner_file: DivePlannerFile) -> DivePlanner {
+        DivePlanner {
+            application_state: Default::default(),
+            dive_planning: DivePrePlanning::new_from_load(dive_planner_file.dive_model),
+            dive_stage: dive_planner_file.dive_stage,
+            dive_information: dive_planner_file.dive_information,
+            dive_results: dive_planner_file.dive_results,
+        }
+    }
+
+    fn convert_to_dive_planner_file(&self) -> DivePlannerFile {
+        DivePlannerFile {
+            dive_model: self
+                .dive_planning
+                .select_dive_model
+                .selected_dive_model
+                .unwrap_or_default(),
+            dive_stage: self.dive_stage,
+            dive_information: self.dive_information.clone(),
+            dive_results: self.dive_results.clone(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
+pub struct DivePlannerFile {
+    pub dive_model: SelectableDiveModel,
     pub dive_stage: DiveStage,
     pub dive_information: DiveInformation,
     pub dive_results: DiveResults,
--- src/models/plan/dive_planning/dive_pre_planning.rs
+++ src/models/plan/dive_planning/dive_pre_planning.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-use crate::models::plan::dive_planning::select_dive_model::SelectDiveModel;
+use crate::{
+    application::states::selectable_dive_model::SelectableDiveModel,
+    models::plan::dive_planning::select_dive_model::SelectDiveModel,
+};
 
 #[derive(Debug, PartialEq, Copy, Clone, Serialize, Deserialize)]
 pub struct DivePrePlanning {
@@ -13,6 +16,17 @@ impl Default for DivePrePlanning {
         Self {
             is_planning: true,
             select_dive_model: Default::default(),
+        }
+    }
+}
+
+impl DivePrePlanning {
+    pub fn new_from_load(selected_dive_model: SelectableDiveModel) -> Self {
+        let mut select_dive_model = SelectDiveModel::default();
+        select_dive_model.selected_dive_model = Some(selected_dive_model);
+        Self {
+            is_planning: false,
+            select_dive_model,
         }
     }
 }
