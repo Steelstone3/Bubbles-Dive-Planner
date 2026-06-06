c00de6552b7f9d1cb377436344a68edd39601740
Skippy <steelstone3@gmail.com>
2026-06-06 20:30:47 +0100

    version-06-06-26

--- src/controllers/dive_profile/decompression.rs
+++ src/controllers/dive_profile/decompression.rs
@@ -1,141 +1,176 @@
-use crate::models::plan::{dive_stage::DiveStage, dive_step::DiveStep};
+// use crate::models::{
+//     application::dive_planner::DivePlanner,
+//     plan::{dive_stage::DiveStage, dive_step::DiveStep},
+// };
 
-impl DiveStage {
-    pub fn calculate_decompression_dive_steps(&self) -> Vec<DiveStep> {
-        let mut dive_steps = vec![];
+// impl DiveStage {
+//     pub fn calculate_decompression_dive_steps(&self) -> DiveStage {
+//         let mut dive_stage = self.clone();
+//         let mut decompression_steps = vec![];
 
-        if self
-            .dive_model
-            .dive_profile
-            .tolerated_surface_pressure
-            .get_dive_ceiling()
-            <= 0.0
-        {
-            return Default::default();
-        }
+//         if self
+//             .dive_model
+//             .dive_profile
+//             .tolerated_surface_pressure
+//             .get_dive_ceiling()
+//             <= 0.0
+//         {
+//             return Default::default();
+//         }
 
-        while self
-            .dive_model
-            .dive_profile
-            .tolerated_surface_pressure
-            .get_dive_ceiling()
-            > 0.0
-        {}
+//         while dive_stage
+//             .dive_model
+//             .dive_profile
+//             .tolerated_surface_pressure
+//             .get_dive_ceiling()
+//             > 0.0
+//         {
+//             match DivePlanner::update_dive_profile(&dive_stage) {
+//                 Some(dive_stage) => {
+//                     decompression_steps.push(dive_stage.calculate_decompression_dive_step())
+//                 }
+//                 None => break,
+//             };
+//         }
 
-        //         while dive_stage.dive_step.depth == nearest_decompression_depth {
-        //     match DivePlanner::update_dive_profile(&dive_stage) {
-        //         Some(dive_stage) => {
-        //             depth = dive_stage.dive_step.depth;
-        //             time += 1;
-        //         }
-        //         None => return None,
-        //     }
-        // }
+//         //         while dive_stage.dive_step.depth == nearest_decompression_depth {
+//         //     match DivePlanner::update_dive_profile(&dive_stage) {
+//         //         Some(dive_stage) => {
+//         //             depth = dive_stage.dive_step.depth;
+//         //             time += 1;
+//         //         }
+//         //         None => return None,
+//         //     }
+//         // }
 
-        // let nearest_decompression_dive_step = DiveStage::find_nearest_decompression_depth(
-        //     self.dive_model
-        //         .dive_profile
-        //         .tolerated_surface_pressure
-        //         .get_dive_ceiling(),
-        // );
+//         // let nearest_decompression_dive_step = DiveStage::find_nearest_decompression_depth(
+//         //     self.dive_model
+//         //         .dive_profile
+//         //         .tolerated_surface_pressure
+//         //         .get_dive_ceiling(),
+//         // );
 
-        // match DiveStage::calculate_decompression_time_at_depth(&self) {
-        //     Some(dive_step) => {
-        //         dive_steps.push(dive_step);
-        //     }
-        //     None => (),
-        // }
+//         // match DiveStage::calculate_decompression_time_at_depth(&self) {
+//         //     Some(dive_step) => {
+//         //         dive_steps.push(dive_step);
+//         //     }
+//         //     None => (),
+//         // }
 
-        dive_steps
-    }
+//         dive_stage
+//     }
 
-    fn calculate_decompression_dive_step(&self) -> DiveStep {
-        let step_interval = 3;
-        let nearest_decompression_depth = (self
-            .dive_model
-            .dive_profile
-            .tolerated_surface_pressure
-            .get_dive_ceiling()
-            / (step_interval as f32))
-            .ceil() as u32
-            * step_interval;
+//     fn calculate_decompression_dive_step(&self) -> DiveStep {
+//         let mut updated_dive_stage = self.clone();
+//         let nearest_decompression_depth = self.find_nearest_decompression_depth();
+//         let mut time = 0;
 
-        DiveStep::new(nearest_decompression_depth, 1)
-    }
+//         while DiveStage::get_dive_ceiling(&updated_dive_stage) < nearest_decompression_depth as f32
+//         {
+//             match DivePlanner::update_dive_profile(&self) {
+//                 Some(dive_stage) => {
+//                     updated_dive_stage = dive_stage;
+//                     time += 1;
+//                 }
+//                 None => (),
+//             }
+//         }
 
-    // fn find_nearest_decompression_depth(dive_ceiling: f32) -> Option<DiveStep> {
-    //     let step_interval = 3;
+//         DiveStep::new(nearest_decompression_depth, time)
+//     }
 
-    //     if dive_ceiling <= 0.0 {
-    //         return None;
-    //     }
+//     fn find_nearest_decompression_depth(&self) -> u32 {
+//         let step_interval = 3;
+//         let nearest_decompression_depth = (self
+//             .dive_model
+//             .dive_profile
+//             .tolerated_surface_pressure
+//             .get_dive_ceiling()
+//             / (step_interval as f32))
+//             .ceil() as u32
+//             * step_interval;
+//         nearest_decompression_depth
+//     }
 
-    //     let nearest_decompression_depth =
-    //         (dive_ceiling / (step_interval as f32)).ceil() as u32 * step_interval;
+//     fn get_dive_ceiling(&self) -> f32 {
+//         self.dive_model
+//             .dive_profile
+//             .tolerated_surface_pressure
+//             .get_dive_ceiling()
+//     }
 
-    //     Some(DiveStep::new(nearest_decompression_depth, 1))
-    // }
+//     // fn find_nearest_decompression_depth(dive_ceiling: f32) -> Option<DiveStep> {
+//     //     let step_interval = 3;
 
-    // fn calculate_decompression_time_at_depth(dive_stage: &DiveStage) -> Option<DiveStep> {
-    //     let mut depth = 0;
-    //     let mut time = 0;
+//     //     if dive_ceiling <= 0.0 {
+//     //         return None;
+//     //     }
 
-    //     let nearest_decompression_depth = match DiveStage::find_nearest_decompression_depth(
-    //         dive_stage
-    //             .dive_model
-    //             .dive_profile
-    //             .tolerated_surface_pressure
-    //             .get_dive_ceiling(),
-    //     ) {
-    //         Some(dive_step) => dive_step.depth,
-    //         None => return None,
-    //     };
+//     //     let nearest_decompression_depth =
+//     //         (dive_ceiling / (step_interval as f32)).ceil() as u32 * step_interval;
 
-    //     while dive_stage.dive_step.depth == nearest_decompression_depth {
-    //         match DivePlanner::update_dive_profile(&dive_stage) {
-    //             Some(dive_stage) => {
-    //                 depth = dive_stage.dive_step.depth;
-    //                 time += 1;
-    //             }
-    //             None => return None,
-    //         }
-    //     }
+//     //     Some(DiveStep::new(nearest_decompression_depth, 1))
+//     // }
 
-    //     Some(DiveStep::new(depth, time))
-    // }
-}
+//     // fn calculate_decompression_time_at_depth(dive_stage: &DiveStage) -> Option<DiveStep> {
+//     //     let mut depth = 0;
+//     //     let mut time = 0;
 
-#[cfg(test)]
-mod dive_stage_should {
-    use crate::{models::plan::dive_step::DiveStep, test::test_fixture::dive_stage_test_fixture};
+//     //     let nearest_decompression_depth = match DiveStage::find_nearest_decompression_depth(
+//     //         dive_stage
+//     //             .dive_model
+//     //             .dive_profile
+//     //             .tolerated_surface_pressure
+//     //             .get_dive_ceiling(),
+//     //     ) {
+//     //         Some(dive_step) => dive_step.depth,
+//     //         None => return None,
+//     //     };
 
-    #[test]
-    fn test_calculate_decompression_dive_step() {
-        // Given
-        let expected_decompression_step_1 = DiveStep::new(6, 1);
-        let expected_decompression_step_2 = DiveStep::new(3, 3);
-        let dive_stage = dive_stage_test_fixture();
+//     //     while dive_stage.dive_step.depth == nearest_decompression_depth {
+//     //         match DivePlanner::update_dive_profile(&dive_stage) {
+//     //             Some(dive_stage) => {
+//     //                 depth = dive_stage.dive_step.depth;
+//     //                 time += 1;
+//     //             }
+//     //             None => return None,
+//     //         }
+//     //     }
 
-        // When
-        let decompression_step_1 = dive_stage.calculate_decompression_dive_step();
-        let decompression_step_2 = dive_stage.calculate_decompression_dive_step();
+//     //     Some(DiveStep::new(depth, time))
+//     // }
+// }
 
-        // Then
-        pretty_assertions::assert_eq!(expected_decompression_step_1, decompression_step_1);
-        pretty_assertions::assert_eq!(expected_decompression_step_2, decompression_step_2);
-    }
+// #[cfg(test)]
+// mod dive_stage_should {
+//     use crate::{
+//         models::plan::{dive_stage::DiveStage, dive_step::DiveStep},
+//         test::test_fixture::dive_stage_test_fixture,
+//     };
 
-    #[test]
-    #[ignore]
-    fn test_calculate_decompression_dive_steps() {
-        // Given
-        let expected_decompression_steps = vec![DiveStep::new(6, 1), DiveStep::new(3, 3)];
-        let dive_stage = dive_stage_test_fixture();
+//     #[test]
+//     fn test_calculate_decompression_dive_step() {
+//         // Given
+//         let expected_decompression_step = DiveStep::new(6, 1);
+//         let dive_stage = dive_stage_test_fixture();
 
-        // When
-        let decompression_steps = dive_stage.calculate_decompression_dive_steps();
+//         // When
+//         let decompression_step = DiveStage::calculate_decompression_dive_step(&dive_stage);
 
-        // Then
-        pretty_assertions::assert_eq!(expected_decompression_steps, decompression_steps);
-    }
-}
+//         // Then
+//         pretty_assertions::assert_eq!(expected_decompression_step, decompression_step);
+//     }
+
+//     #[test]
+//     fn test_calculate_decompression_dive_steps() {
+//         // Given
+//         let expected_decompression_steps = vec![DiveStep::new(6, 1), DiveStep::new(3, 3)];
+//         let dive_stage = dive_stage_test_fixture();
+
+//         // When
+//         let decompression_steps = dive_stage.calculate_decompression_dive_steps();
+
+//         // Then
+//         pretty_assertions::assert_eq!(expected_decompression_steps, decompression_steps);
+//     }
+// }
