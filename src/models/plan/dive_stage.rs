296fb4ccd2f02270e64788caadc80cb93107f689
Skippy <steelstone3@gmail.com>
2026-06-13 08:15:32 +0100

    version-13-06-26

--- src/models/plan/dive_stage.rs
+++ src/models/plan/dive_stage.rs
@@ -1,4 +1,6 @@
-use crate::models::plan::{cylinders::cylinder::Cylinder, dive_model::DiveModel, dive_step::DiveStep};
+use crate::models::plan::{
+    cylinders::cylinder::Cylinder, dive_model::DiveModel, dive_step::DiveStep,
+};
 use serde::{Deserialize, Serialize};
 use std::collections::VecDeque;
 
